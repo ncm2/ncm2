6d6c80d6e6b52270fd20a8986fc8c1b28dcfd239
roxma <l>
2017-01-30 19:42:56 +0800

    move commen markdown code into one place

--- autoload/cm/sources/cm_jedi.py
+++ autoload/cm/sources/cm_jedi.py
@@ -8,6 +8,7 @@ import re
 import logging
 import jedi
 from neovim import attach, setup_logging
+import cm_utils
 
 logger = logging.getLogger(__name__)
 
@@ -38,7 +39,7 @@ class Handler:
         src = "\n".join(self._nvim.current.buffer[:])
 
         if filetype=='markdown':
-            result = get_markdown_python_block_info(src,lnum,col)
+            result = cm_utils.check_markdown_code_block(src,['python'],lnum, col)
             logger.info('try markdown, %s,%s,%s, result: %s', src, col, col, result)
             if result is None:
                 return
@@ -103,74 +104,4 @@ class Handler:
             params=[param.description for param in signature.params]
             signature_text = signature.name + '(' + ', '.join(params) + ')'
         return signature_text
-
-def get_markdown_python_block_info(doc,lnum,col):
-
-    try:
-        import mistune
-
-        # hack the lexer to find this markdown code block
-        class HackBLockLexer(mistune.BlockLexer):
-            def parse(self, text, rules=None):
-                text = text.rstrip('\n')
-                if not rules:
-                    rules = self.default_rules
-                def manipulate(text,pos):
-                    for key in rules:
-                        rule = getattr(self.rules, key)
-                        m = rule.match(text)
-                        if not m:
-                            continue
-                        if (key=='fences' 
-                            and m.group(2)=='python'
-                            and pos+m.start(3) <= self.cm_cur_pos 
-                            and pos+len(m.group(0)) > self.cm_cur_pos
-                            ):
-                            self.cm_current_py_info = dict(src=text[m.start(3):],
-                                                           pos=self.cm_cur_pos-(pos+m.start(3)))
-                            logger.info('group: %s', m.group(0))
-                        getattr(self, 'parse_%s' % key)(m)
-                        return m
-                    return False  # pragma: no cover
-                pos = 0
-                while text:
-                    m = manipulate(text,pos)
-                    if m is not False:
-                        pos+=len(m.group(0))
-                        text = text[len(m.group(0)):]
-                        continue
-                    if text:  # pragma: no cover
-                        raise RuntimeError('Infinite loop at: %s' % text)
-                return self.tokens
-
-
-        block = HackBLockLexer()
-        block.cm_current_py_info = None
-
-        # curpos
-        lines = doc.split('\n')
-        pos = 0
-        for i in range(lnum-1):
-            pos += len(lines[i])+1
-        pos += col-1
-
-        block.cm_cur_pos = pos
-        mistune.markdown(doc,block=block)
-
-        if block.cm_current_py_info:
-            pos = block.cm_current_py_info['pos']
-            src = block.cm_current_py_info['src']
-            p = 0
-            for idx,line in enumerate(src.split("\n")):
-                if p<=pos and (p+len(line))>=pos:
-                    block.cm_current_py_info['lnum'] = idx+1
-                    block.cm_current_py_info['col'] = pos-p+1
-                    return block.cm_current_py_info
-                p += len(line)+1
-
-        return None
-
-    except Exception as ex:
-        logger.info('exception, %s', ex)
-        return None
 
