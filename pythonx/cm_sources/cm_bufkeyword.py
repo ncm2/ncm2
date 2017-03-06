e08c1d0126e6b512d4ddb666ea9e534c52fd5148
roxma <roxma@qq.com>
2017-03-06 23:15:17 +0800

    bufkeyword fix, #40

--- pythonx/cm_sources/cm_bufkeyword.py
+++ pythonx/cm_sources/cm_bufkeyword.py
@@ -8,61 +8,75 @@ from cm import register_source, getLogger, get_matcher
 register_source(name='cm-bufkeyword',
                    priority=5,
                    abbreviation='Key',
-                   events=['CursorHold','CursorHoldI','BufEnter','BufWritePost','TextChangedI'],)
+                   events=['InsertEnter', 'BufEnter'],)
 
 import os
 import re
+import cm_default
 
 logger = getLogger(__name__)
 
 class Source:
 
     def __init__(self,nvim):
-
         self._nvim = nvim
         self._words = set()
-
-        self._split_pattern = r'[^\w#]+'
-        self._kw_pattern = r'[\w#]'
-
         self._last_ctx = None
-
-        self.refresh_keyword()
+        self.refresh_keyword(nvim.eval('cm#context()'))
 
     def cm_event(self,event,ctx,*args):
-        if event=="TextChangedI":
-            if ctx['typed'] and re.match(self._kw_pattern,ctx['typed'][-1]) is None:
-                self.refresh_keyword_incr(ctx['typed'])
-        elif event in ['CursorHold','CursorHoldI','BufEnter','BufWritePost']:
-            if self._last_ctx == ctx:
-                return
-            self._last_ctx = ctx
-            self.refresh_keyword()
+        if self._last_ctx and (self._last_ctx['changedtick'] == ctx['changedtick']):
+            return
+        self._last_ctx = ctx
+        self.refresh_keyword(ctx)
 
+    def refresh_keyword(self,ctx,all=True,expand=50):
+        word_pattern = cm_default.word_pattern(ctx)
+        compiled = re.compile(word_pattern)
+        logger.info('refreshing_keyword, word_pattern [%s]', word_pattern)
 
-    def refresh_keyword(self):
-        logger.info('refresh_keyword')
-        pat = re.compile(self._split_pattern)
-        self._words = set()
-        for line in self._nvim.current.buffer[:]:
-            for word in re.split(pat,line):
-                self._words.add(word)
+        buffer = self._nvim.current.buffer
+
+        if all:
+            self._words = set()
+            begin = 0
+            end = len(buffer)
+        else:
+            begin = max(ctx['lnum']-50,0)
+            end = min(ctx['lnum']+50,len(buffer))
+
+        logger.info('keyword refresh begin, current count: %s', len(self._words))
+
+        cur_lnum = ctx['lnum']
+        cur_col = ctx['col']
+
+        step = 1000
+        for num in range(begin,end,step):
+            lines = buffer[num:num+step]
+            # convert 0 base to 1 base
+            lnum = num+1
+            for line in lines:
+                if lnum == cur_lnum:
+                    for word in compiled.finditer(line):
+                        span = word.span()
+                        # filter-out the word at current cursor
+                        if (cur_col>=span[0]+1) and (cur_col-1<=span[1]+1):
+                            continue
+                        self._words.add(word.group())
+                else:
+                    for word in compiled.finditer(line):
+                        self._words.add(word.group())
+                lnum += 1
 
         logger.info('keyword refresh complete, count: %s', len(self._words))
 
-    def refresh_keyword_incr(self,line):
-
-        logger.info('refresh_keyword_incr')
-        pat = re.compile(self._split_pattern)
-
-        for word in re.split(pat,line):
-            self._words.add(word)
-
-        logger.info('keyword refresh incr complete, count: %s', len(self._words))
-
     def cm_refresh(self,info,ctx):
 
+        # incremental refresh
+        self.refresh_keyword(ctx,False)
+
         matches = (dict(word=word,icase=1)  for word in self._words)
+
         matches = get_matcher(self._nvim).process(info, ctx, ctx['startcol'], matches)
 
         # cm#complete(src, context, startcol, matches)
